6b670f933ef16d410ed5004b800c7b9f08e092d1
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-09-23 16:48:19 -0700

    mutator test

--- inst/tests/test-fun.R
+++ inst/tests/test-fun.R
@@ -60,5 +60,16 @@ test_that("summarizer with missing names", {
    expect_equal(f(10, 2, 3), quickdf(list(`x/y`=5, `2 * z`=6)))
 })
 
-test_that("fun equivalent of mutate", {
+test_that("mutator", {
+  f <- mutator(xy=x+y, xxyy=xy^2, z+x)
+  df <- data.frame(x=1:10, y=sqrt(1:10), z=11:20)
+
+  expect_equal(arrange(splat(f)(df)),
+               data.frame(x=1:10,
+                          xxyy = (1:10 + sqrt(1:10))^2,
+                          xy = 1:10 + sqrt(1:10),
+                          y = 1:10,
+                          z=11:20,
+                          `z+x` = ))
 })
+
