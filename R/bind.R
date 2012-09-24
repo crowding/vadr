20585db3a6cc55020fed67c1a417d329dc54ac8d
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-09-23 20:41:00 -0700

    Changing to square brackets for destructuring bind
    Here's the problem I run up against: in the process of <- assigning,
    the first argument is clobbered by R and replaced with `*tmp*`. Its
    argument name is clobbered too. I think using square brackets will let
    me get around that.

--- R/bind.R
+++ R/bind.R
@@ -1,13 +1,50 @@
+##' Unpack a list and assign to multiple variables.
+##'
+##' This is a "destructuring bind" for R. It can be used to unpack
+##' lists into different values, or achieve the effect of multiple
+##' return values from a function.
+##'
+##' @examples
+##' # For a simple example,
+##' bind(a=x, b=y) <- c(a="foo", b="bar")
+##' x # -> "foo"
+##' y # -> "bar"
+##'
+##' # Note that the assigned-to variable is on the _right_ side of each
+##' # equals in the arglist. This is admittedly awkward but is the best
+##' # way to remain consistent with R's argument-binding semantics.
+##'
+##' # Element to variable matching happens according to R's argument
+##' # binding rules, can be nested, and complicated assignemnt targets
+##' # can be used. The following shows off a complicated unpacking:
+##'
+##' bind[a=x, ...=bind[aa=xx, bb=yy], b=y, cc=bind[a=zz[2], b=zz[1]]] <-
+##'   list(a=1, b="two", aa="eleven", bb=22, cc=c(a=1, b=2))
+##' x
+##' y
+##' xx
+##' yy <- 22
+##' zz <- c(2,1)
+##'
+##' # x <- 1; y <- "two", xx <- "eleve
+##'
+##' @param ...
+##' @param .envir The environment to bind in (defaults to the caller).
+##' @aliases bind<- [<-.bind <-.bind
+##' @return The list that was originally unpacked.
+##' @author Peter Meilstrup
 ##' @export
-bind <- function(..., .envir=parent.frame()) {
-  print(match.args())
-  print(match.call())
-  #nothing
+bind <- "use bind[a=x, b=y] <- c(a=1,b=2) to do parallel assignment."
+class(bind) <- "bind"
+
+`[<-.bind` <- function(..., .envir=parent.frame()) {
+  #now the problem is to get the "calling frame" all the way thorough
+  #whatever dispatch mechanism is in place.
+  print(eval(substitute(alist(...))))
+  stop("not written!")
 }
 
 ##' @export
-`bind<-` <- function(..., .envir=parent.frame()) {
-  print(match.args())
-  print(match.call())
-  #nothing
+`[.bind` <- function(tmp) {
+  stop("bind[...] must be used as the target of an assignment.");
 }
