b8d6f543b6a88252229ba1d2be8972630a123705
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-10-01 01:04:20 -0700

    a start on the dm*ply core function

--- R/dm_ply.R
+++ R/dm_ply.R
@@ -1,3 +1,6 @@
+splitter_d <- plyr:::splitter_d
+list_to_dataframe <- plyr:::list_to_dataframe
+
 ##' Split data frame, apply columns to arguments of a function, and return results in a data frame.
 ##'
 ##' For each subset of a data frame, apply a multiple-argument
@@ -18,18 +21,11 @@
 ##' base2 <- dmdply(baseball, .(id),
 ##'     mutator(career_year=year - min(year) + 1))
 dmdply <- function(.data, .variables, .fun=NULL, ..., .progress="none", .drop=TRUE, .parallel=FALSE) {
-  stop("not written")
-}
+  pieces <- splitter_d(.data, .variables, drop = .drop)
+  res <- dm_ply_worker(.data=.data, .pieces=pieces, .fun=.fun, ..., .progress="none", .drop=.drop, .parallel=.parallel)
+  list_to_dataframe(res, attr(.data, "split_labels"))
 
-##' Split data frame, apply columns to arguments of a function, and return results in a list.
-##'
-##' For each subset of a data frame, apply the columns of the data
-##' frame to a multiple-argument function, then combine the results
-##' into a data frame.
-##'
-##' @template dm-
-##' @template -l
-dmlply <- function(.data, .variables, fun=NULL, ..., .progress="none") {
+  #now I still need to re-apply ths split columns.
   stop("not written")
 }
 
@@ -47,5 +43,57 @@ dmlply <- function(.data, .variables, fun=NULL, ..., .progress="none") {
 ##' daply(baseball[, c(6:9)], .(baseball$year), argwise(mean))
 ##' daply(baseball, .(year), argwise(mean, .args=names(df[6:9])))
 dmaply <- function(.data, .variables, .fun=NULL, ..., .progress="none", .drop_i = TRUE, .drop_o = TRUE, .parallel=TRUE) {
+  pieces <- splitter_d(.data, .variables, drop = .drop)
+  res <- dm_ply_worker(.data=.data, .pieces=pieces, .fun=.fun, ..., .progress=progress, .drop=drop_o, .parallel=.parallel)
+  list_to_array(res, attr(.data, "split_labels"), .drop=.drop_o)
+}
+
+##' Split data frame, apply columns to arguments of a function, and return results in a list.
+##'
+##' For each subset of a data frame, apply the columns of the data
+##' frame to a multiple-argument function, then combine the results
+##' into a data frame.
+##'
+##' @template dm-
+##' @template -l
+##' @export
+dmlply <- function(.data, .variables, .fun=NULL, ..., .progress="none", drop=TRUE, .parallel=TRYE) {
+  pieces <- splitter_d(.data, .variables, drop = .drop)
+  res <- dm_ply_worker(.data=.data, .pieces=pieces, .fun=.fun, ..., .progress="none", .drop, .parallel=.parallel)
+}
+
+dm_ply_worker <- function(.data, .pieces, .fun=NULL, ..., .progress="none", .drop, .parallel=TRUE) {
+  ##now having the splits, we look at the function...
+  if (is.null(.fun)) fun <- mutator();
+  if (is.character(.fun) || is.list(.fun)) .fun <- each(.fun)
+  if (!is.function(.fun)) stop(".fun is not a function.")
+
+  arg.names <- names(formals(.fun))
+  args.used <- intersect(names(.data), names(formals(.fun)))
+  if ("..." %in% arg.names) {
+    rest.args <- setdiff(names(.data), names(formals(.fun)))
+  } else {
+    rest.args <- character(0)
+  }
+
+  #construct our function call. The variable "index" will change.
+  args <- lapply(c(args.used, rest.args), function(col) substitute(.data$column[i], list(column=col)))
+  names(args) <- c(args.used, rest.args)
+  f <- eval(substitute(function(i) c, list(c=as.call(c(as.name(".fun"), args)))))
+
+  #FIXME this should fall back to llply so that parallel can be used.
+  test <- lapply(.pieces$index, f)
+}
+
+##' Split data frame, apply columns to arguments of a function, and discard results.
+##'
+##' For each subset of a data frame, apply its columns to a multiple-argument function.
+##'
+##' @template dm-
+##' @template -_
+##' @export
+dm_ply <- function(.data, .variables, .fun=NULL, ..., .progress="none", .drop=TRUE, .parallel=FALSE) {
+  pieces <- splitter_d(.data, .variables, drop = .drop)
   stop("not written")
+  invisible()
 }
---
+++ man-roxygen/-_.r
@@ -0,0 +1,7 @@
+#' @section Output:
+#' This function discards its output. THis is useful for functions
+#' that you are calling purely for their side effects like display plots
+#' and saving output.
+#'
+#' @return NULL
+#' @family null output
