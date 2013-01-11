7c6cb3838ab2bc2dacfc0f34f102438ee626ceb3
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-01-11 15:32:53 -0800

    remove duplicate ammoc test

--- tests/test-ammoc.R
+++
@@ -1,9 +0,0 @@
-context("ammoc")
-
-test_that("evaluates first", {
-  y <- "hello"
-  res <- ammoc(x <- y, rm(y))
-  expect_false(exists("y"))
-  expect_equal(x, "hello")
-  expect_equal(res, "hello")
-}
