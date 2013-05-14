27a896c32b0316e0d3371c7736433ee944a49e73
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-05-14 05:01:26 -0700

    test syntax for chain argument name and chain intermediata variables

--- inst/tests/test-chain.R
+++ inst/tests/test-chain.R
@@ -37,14 +37,39 @@ test_that('chain lexical scope', {
   expect_equal(dist, check)
 })
 
-test_that('chain intermediate result', {
-  #nothing here yet. Think about the syntax.
+test_that('mkchain placeholder', {
+  f <- mkchain[foo](apply(2, diff), foo^2, rowSums, sqrt, cumsum)
+  expect_equal(f(path), check)
 })
 
 test_that('chain placeholder', {
-#  dist <- chain(foo=P, apply(2,diff), foo^2, rowSums, sqrt, cumsum)
+  dist <- chain[foo](path, apply(2,diff), foo^2, rowSums, sqrt, cumsum)
+  expect_equal(dist, check)
 })
 
-test_that('mkchain placeholder', {
-  #mkchain(foo=., apply(2, diff), foo^2, rowSums, sqrt, sum)
+test_that('chain/mkchain arguments', {
+  data <- c(13, 32, 54, 68, 12, 31, 14, 31,  5,  9)
+  f <- mkchain[x, threshold=20](x>threshold, sum)
+  expect_equal(f(data), sum(data > 20))
+  expect_equal(f(data, 60), sum(data > 60))
+
+  #and for mkchain
+  expect_equal(chain[x, threshold=20](data, x>threshold, sum))
+  #no way to change "threshold" in the chain form but that's ok
 })
+
+test_that('chain remembers intermediate result', {
+  test <- (function(.) {
+    . <- foo <- .
+    . <- apply(., 2, diff)
+    . <- foo^2
+    . <- rowSums(.)
+    . <- sqrt(.)
+    . <- cumsum(.)
+    . <- foo/.
+  })(path)
+  #should be equiv.
+  dist <- chain(foo=path, apply(2,diff), foo^2, rowSums, sqrt, cumsum, foo/.)
+  expectEqual(foo, test)
+})
+
