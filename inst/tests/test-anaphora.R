e43704b836303b007aaa783e478414ca06ca0ee3
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-08-18 01:22:45 -0700

    more anaphora ideas/incomplete tests

--- inst/tests/test-anaphora.R
+++ inst/tests/test-anaphora.R
@@ -1,35 +1,58 @@
 #anaphora -- functions that do "something" with a (complex expression)
-#input without repeating hte conplex expression
-
-library(testthat)
+#input without repeating the complex expression
 context("anaphora")
 
-test_that("put() changes a subelement of a structure, returning modified structure", {
+test_that("put() changes a subset to a value", {
   x <- 1:10
-  y <- put(x[1], 4)
+  y <- put(x, on[1], 4)
   expect_equal(x, 1:10)
   expect_equal(y, c(4, 2:10))
   withNames(1:10, letters(1:10))
 })
 
-test_that("alter() runs subelements of s structure through a chain", {
+test_that("alter() sets a sebset to the chain of the subset", {
   x <- structure(1:5, letters[1:5])
-  y <- alter(names(x[5]), toupper)
+  y <- alter(x, names(on[5]), toupper)
   y <- names(x(5))
 })
 
-test_that("on() specifies target put/alter target (if computed value)", {
-  x <- put(names(on(1:10)), letters(1:10))
+test_that("inject() sets a subelement of a value to the chain of the value", {
+  x <- inject(1:10, names, letters[], rev)
+})
+
+test_that("with_attrs is a list of a thing and its attributes", {
+  
+})
+
+test_that(paste("rebind() is a functional equiv. of bind(), "
+                "outputting a list/data frame of the same class?"), {
+  
+})
+
+test_that("`on` used as placeholder target put/alter/inject target (if computed value)", {
+  x <- put(1:10, names(on(1:10)), letters(1:10))
   expect_equal(x, structure(1:10, names=letters(1:10)))
 
   y <- alter(names(on(rev(x))), toupper)
   expect_equal(y, structure(10:1, names=letters(10:1)))
 })
 
-test_that("via<- allows values to be modified, in the manner of chain()", {
+
+
+test_that("via<-(x), sets x to chain of its arguments", {
   x <- 1:5
   via(names[x[5]], toupper) <- letters[5]
   expect_equal(x, c(1,2,3,4,E=5))
+})
+
+test_that("check() chains a value through conditions and throws errors", {
+  structure(1:5, names=letters(1:5))
+})
+
+test_that("checked<-() asserts that the provided value passes a chain with TRUE", {
+  check(x, names, !any(.="")) <- structure(1:5, names=letters(1:5))
+  expect_equal(x, structure(1:5, names=letters(1:5)))
+  expect_error( check(x, .>0, all) <- c(1, -1) )
 })
 
 test_that("default<- lets you provide a default value from bind[]", {
@@ -65,10 +88,4 @@ test_that("default<- allows there to be missing matches as well", {
     expect_equal(a, 5)
     expect_equal(b, 3)
   })
-})
-  
-test_that("<- asserts that the provided value passes a chain with TRUE", {
-  check(x, names, !any(.="")) <- structure(1:5, names=letters(1:5))
-  expect_equal(x, structure(1:5, names=letters(1:5)))
-  expect_error( check(x, .>0, all) <- c(1, -1) )
 })
