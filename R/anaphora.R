83bc298b0815c57d0f7f7cd50392b7d64c47c9d3
Peter Meilstrup <peter.meilstrup@gmail.com>
2014-01-03 18:11:31 -0800

    "put" anaphor

--- R/anaphora.R
+++ R/anaphora.R
@@ -5,8 +5,10 @@
 #' @param x A variable to assign to,
 #' @param y An expression, interpreted in the manner of \code{\link{chain}}.
 #' @return The rvalue of the assignment.
+#' @rdname augment
+#' @aliases %<~%
 #' @author Peter Meilstrup
-#' @seealso chain
+#' @seealso chain put
 #' @export
 #' @examples
 #' library(stringr)
@@ -24,3 +26,87 @@
     qq(.(x) <- .(y)(.(x)))
   }
 })
+
+#' Modify part of a value.
+#'
+#' The macro \code{put} provides syntax for modifying part of an
+#' object in a functional context (i.e. creating a modified value
+#' without necessarily modifying without binding the result to a
+#' name.) Unlike \code{link{<-}}, the value of the expression is the
+#' modified object, not the value that was injected. This is
+#' particularly useful in combination with 'chain.'
+#'
+#' Normal subassignment in R is effectively a macro, one which turns a
+#' statement like
+#'
+#' \code{names(x)[1] <- "head"}
+#'
+#' into something like
+#'
+#' \code{x <- `names<-`(x, `[<-`(names(x), "head", 1))}
+#'
+#' However even this explanation is misleading, because the value
+#' returned from a subassignment is the value applied, not the value
+#' assigned. Consider calling a function with a modification of an
+#' existing value:
+#'
+#' \code{do_something(names(x)[1] <- "head")}
+#'
+#' This actually does the equivalent of \code{do_something("head")},
+#' rather than pass a modified \code{x} to \code{do_something}.
+#'
+#' Using \code{put}, one can write:
+#'
+#' \code{do_something(put(x, names[1], "head"))}
+#'
+#' \put() and friends are particularly useful in conjunction with
+#' \code{\link{chain}}.
+#'
+#' @param it A value.
+#' @param subset An expression; this is interpreted literally if the
+#' symbol \code{it} is used, otherwise \code{it} is injected as far down the
+#' leftmost arguments of the expression as possible. (Thus
+#' \code{names} is interpreted as {names(it)}, and \code{names[1]} as
+#' \code{names(it)[1]}.)
+#' @return The modified value.
+#' @author Peter Meilstrup
+#' @seealso chain
+#' @aliases put
+#' @rdname put
+#' @export
+#' @examples
+#' put(1:10, names, letters[1:10])
+#' x <- 1:10
+#' put(x, it[1], 4)
+#' put(x, names[4], 'a')
+#' x #x is unchanged
+put <- macro(function(it, subset, value) {
+  qq( (function(it)
+       {
+         .(address_expand(quote(it), subset)) <- .(value)
+         it
+       }
+      )( .(it) )
+    )
+})
+
+address_expand <- function(arg, address) {
+  if (as.character(arg) %in% all.names(address)) {
+    address
+  } else {
+    address_inject <- function(x) {
+      if (is.call(x)) {
+        x <- as.list(x)
+        if (length(x) >= 2) {
+          as.call(c(x[1], address_inject(x[[2]]), x[c(-1,-2)]))
+        }
+        else {
+          as.call(c(as.list(x), list(arg)))
+        }
+      } else {
+        as.call(list(x, arg))
+      }
+    }
+    address_inject(address)
+  }
+}
--- inst/tests/test-anaphora.R
+++ inst/tests/test-anaphora.R
@@ -18,88 +18,78 @@ test_that("Modification assignment", {
 
 test_that("put() changes a subset to a value", {
   x <- 1:10
-  y <- put(x, on[1], 4)
-  expect_equal(x, 1:10)
+  z <- put(1:10, names, letters[1:10])
+  y <- put(x, it[1], 4)
+  expect_equal(z, structure(1:10, names=letters[1:10]))
   expect_equal(y, c(4, 2:10))
-  withNames(1:10, letters(1:10))
+  expect_equal(x, 1:10)
 })
 
-test_that("alter() sets a sebset to the chain of the subset", {
-  x <- structure(1:5, letters[1:5])
-  y <- alter(x, names(on[5]), toupper)
-  y <- names(x(5))
-})
+## test_that("alter() sets a sebset to the chain of the subset", {
+##   x <- structure(1:5, letters[1:5])
+##   y <- alter(x, names(it)[5], toupper)
+##   x <- alter(x, names(it[5]), toupper)
+##   x <- alter(x, names[5], toupper)
+## })
 
-test_that("inject() sets a subelement of a value to the chain of the value", {
-  x <- inject(1:10, names, letters[], rev)
-})
+## test_that("inject() sets a subelement of a value to the chain of the value", {
+##   x <- inject(1:10, names, letters[], rev)
+## })
 
-test_that("with_attrs is a list of a thing and its attributes", {
-  
-})
+## test_that("with_attrs is a list of a thing and its attributes", {
+## })
 
-test_that(paste("rebind() is a functional equiv. of bind(), "
-                "outputting a list/data frame of the same class?"), {
-  
-})
+## test_that(paste("rebind() is a functional equiv. of bind(), "
+##                 "outputting a list/data frame of the same class?"), {  
+## })
 
-test_that("`on` used as placeholder target put/alter/inject target (if computed value)", {
-  x <- put(1:10, names(on(1:10)), letters(1:10))
-  expect_equal(x, structure(1:10, names=letters(1:10)))
+## test_that("via<-(x), sets x to chain of its arguments", {
+##   x <- 1:5
+##   via(names[x[5]], toupper) <- letters[5]
+##   expect_equal(x, c(1,2,3,4,E=5))
+## })
 
-  y <- alter(names(on(rev(x))), toupper)
-  expect_equal(y, structure(10:1, names=letters(10:1)))
-})
+## test_that("check() chains a value through conditions and throws errors", {
+##   structure(1:5, names=letters(1:5))
+## })
 
+## test_that("checked<-() asserts that the provided value passes a chain with TRUE", {
+##   check(x, names, !any(.="")) <- structure(1:5, names=letters(1:5))
+##   expect_equal(x, structure(1:5, names=letters(1:5)))
+##   expect_error( check(x, .>0, all) <- c(1, -1) )
+## })
 
+## test_that("default<- lets you provide a default value from bind[]", {
+##   local({
+##     bind[a = default(a, 5), default(b, 5)] <- list(a=4, b=3)
+##     expect_equal(a, 4)
+##     expect_equal(b, 3)
+##   })
+##   local({
+##     bind[a = default(a, 5), default(b, 5)] <-
+##         list(a=missing_value(), b=missing_value())
+##     expect_equal(a, 5)
+##     expect_equal(b, 5)
+##   })
+##   local({
+##     bind[default(a, 5), b] <- list(missing_value(), 4)
+##     expect_equal(a, 5)
+##     expect_equal(b, 4)
+##   })
+## })
 
-test_that("via<-(x), sets x to chain of its arguments", {
-  x <- 1:5
-  via(names[x[5]], toupper) <- letters[5]
-  expect_equal(x, c(1,2,3,4,E=5))
-})
-
-test_that("check() chains a value through conditions and throws errors", {
-  structure(1:5, names=letters(1:5))
-})
-
-test_that("checked<-() asserts that the provided value passes a chain with TRUE", {
-  check(x, names, !any(.="")) <- structure(1:5, names=letters(1:5))
-  expect_equal(x, structure(1:5, names=letters(1:5)))
-  expect_error( check(x, .>0, all) <- c(1, -1) )
-})
-
-test_that("default<- lets you provide a default value from bind[]", {
-  local({
-    bind[a = default(a, 5), default(b, 5)] <- list(a=4, b=3)
-    expect_equal(a, 4)
-    expect_equal(b, 3)
-  })
-  local({
-    bind[a = default(a, 5), default(b, 5)] <-
-        list(a=missing_value(), b=missing_value())
-    expect_equal(a, 5)
-    expect_equal(b, 5)
-  })
-  local({
-    bind[default(a, 5), b] <- list(missing_value(), 4)
-    expect_equal(a, 5)
-    expect_equal(b, 4)
-  })
-})
-
-test_that("default<- allows there to be missing matches as well", {
-  local({
-    expect_error({
-      bind(a, b) <- list(a=4)
-    })
-  })
-  local({
-    bind(a, b=2) <- list(a=4)
-  })
-  local({
-    bind[a = default(a, 5), default(b, 2)] <- list(b=3)
-    expect_equal(a, 5)
-    expect_equal(b, 3)
-  })
-})
+## test_that("default<- allows there to be missing matches as well", {
+##   local({
+##     expect_error({
+##       bind(a, b) <- list(a=4)
+##     })
+##   })
+##   local({
+##     bind(a, b=2) <- list(a=4)
+##   })
+##   local({
+##     bind[a = default(a, 5), default(b, 2)] <- list(b=3)
+##     expect_equal(a, 5)
+##     expect_equal(b, 3)
+##   })
+## })
