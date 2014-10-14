548224618c6ed60fa2793f058937a487ec7e48d9
Peter Meilstrup <peter.meilstrup@gmail.com>
2014-10-13 18:59:36 -0700

    fun documentatation

--- R/fun.R
+++ R/fun.R
@@ -1,38 +1,20 @@
-##' A very compact way to define a function.
+##' A compact way to define a function.
 ##'
 ##' \code{fun} captures its first argument unevaluated and turns it
 ##' into a function. Every name used in the expression becomes an
 ##' argument, unless it looks like a function call. If you don't
 ##' intend to capture a particular variable, you can not provide it,
 ##' and it will use a default value that pulls from the enclosing
-##' scope. For example:
-##'
-##' \code{ > f <- fun(x/y)
-##' > f(10,2)
-##' [1] 5
-##' > f
-##' function (x = eval(quote(x),<environment>),
-##'           y = eval(quote(y),<environment>) {
-##'   x/y
-##' }
-##'
-##' "\code{fun}" is used with "\code{dm_ply}" the way that "\code{with}" is
-##' used with  "\link[plyr]{d_ply}".
+##' scope.
 ##'
 ##' "\code{...}" is supported in the function definitions and should
 ##' behave as you expect.
 ##'
+##' @usage fun(expr, .all.names=FALSE)
+##'
 ##' @param expr The expression to use as the function's body.
 ##'
-##' @param .all.names Defaults to \code{FALSE}, in which case the
-##' formal arguments of the function are only the parts of the
-##' expression that "look like" variables (i.e. names that do not head
-##' calls). If set to \code{TRUE}, all symbols are wrapped in formal
-##' arguments. This includes all things that R treats as calls, like
-##' \code{`+`}, \code{`(`} and \code{`{`}. Note that "in the order of
-##' appearance" means in the s-expression order, so that
-##' \code{"fun(x+y, .all.names=TRUE)"} will have arguments named "+",
-##' "x" and "y" in that order.
+##' @param .all.names Whether to include call heads as parameters.
 ##'
 ##' @return A newly constructed function.
 ##'
@@ -45,6 +27,15 @@
 ##' @author Peter Meilstrup
 ##'
 ##' @export
+##' @examples
+##' f <- fun(x/y)
+##' f(10,2)  # prints 5
+##' f
+##' # function (x = eval(quote(x),..fun_envir),
+##' #           y = eval(quote(y),..fun_envir) {
+##' #   x/y
+##' # }
+
 fun <- macro(function(body, .all.names=FALSE) {
   names <- all.names(body, functions=.all.names, unique=TRUE)
   arglist <- qqply(`.(name)`=eval(quote(`.(name)`), ..fun_envir))(name=names)
